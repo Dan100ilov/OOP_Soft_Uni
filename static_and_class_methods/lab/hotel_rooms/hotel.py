24a062bfc63554788c6e39fa7aa7b4353e0ecdbe
Dan100ilov <115982892+Dan100ilov@users.noreply.github.com>
2023-03-10 10:54:23 +0200

    Add files via upload

---
+++ static_and_class_methods/lab/hotel_rooms/hotel.py
@@ -0,0 +1,36 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+
+    @property
+    def guests(self):
+        return sum([room.guests for room in self.rooms])
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = [room for room in self.rooms if room.number == room_number][0]
+        return room.take_room(people)
+
+    def free_room(self, room_number):
+        room = [room for room in self.rooms if room.number == room_number][0]
+        return room.free_room(room_number)
+
+    def status(self):
+        result = f"Hotel {self.name} has {self.guests} total guests"
+        return result
+
+    def status(self):
+        free_rooms = [str(room.number) for room in self.rooms if not room.is_taken]
+        taken_rooms = [str(room.number) for room in self.rooms if room.is_taken]
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join(free_rooms)}\n" \
+               f"Taken rooms: {', '.join(taken_rooms)}"
+
+
